e22bb2c9066d48e22f51fff8bfd351b97dfd3d2f
hermayani2786 <herma.yani2786@gmail.com>
2026-05-12 18:48:43 +0700

    membuat class anak

--- anak.h
+++ anak.h
@@ -5,3 +5,14 @@
 #include <string>
 
 using namespace std;
+
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+     ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
